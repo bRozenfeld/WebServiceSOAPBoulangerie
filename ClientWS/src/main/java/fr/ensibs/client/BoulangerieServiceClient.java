f9ca5d2bd290ede8cb4460f0c7a1f95c135ec756
brozenfeld <benjamin.rozenfeld@protonmail.com>
2020-02-14 10:30:37 +0100

    Implementing LogIn

--- ClientWS/src/main/java/fr/ensibs/client/BoulangerieServiceClient.java
+++ ClientWS/src/main/java/fr/ensibs/client/BoulangerieServiceClient.java
@@ -1,5 +1,89 @@
 package fr.ensibs.client;
 
+import java.util.Scanner;
+
 public class BoulangerieServiceClient {
-    
+
+    private static boolean isAuth;
+    private static String token;
+    private static UserService_Service userService;
+    private static UserService userPort;
+
+    public static void main(String[] args) {
+        BoulangerieServiceClient instance = new BoulangerieServiceClient();
+        instance.run();
+    }
+
+    public BoulangerieServiceClient() {
+        token = null;
+        userService = new UserService_Service();
+        userPort = userService.getUserPort();
+    }
+
+    public void run() {
+        System.out.println("Hello");
+        System.out.println("LIST OF COMMANDS: ");
+        System.out.println("\tSIGNUP <username> <password> <'customer'|'admin'>");
+        System.out.println("\tLOGIN <username> <password>");
+        System.out.println("\tLOGOUT");
+        System.out.println("\tQUIT");
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        while(!line.equals("quit") && !line.equals("QUIT")) {
+            String[] command = line.split(" ");
+            switch(command[0]) {
+                case "signup":
+                case "SIGNUP":
+                    if(command.length == 4 && (command[3].equals("customer") || command[3].equals("admin"))) {
+                        signUp(command[1], command[2], command[3]);
+                    } else { System.out.println("Unknown operation"); }
+                    break;
+                case "login":
+                case "LOGIN":
+                    if(command.length == 3) {
+                        logIn(command[1], command[2]);
+                    } else { System.out.println("Unknown operation"); }
+                    break;
+                default:
+                    System.out.println("Unknown command: \"" + command[0] + "\"");
+            }
+            line = scanner.nextLine();
+        }
+        quit();
+    }
+
+    public void quit() {
+        System.exit(1);
+    }
+
+    /**
+     * Add a new user to the database
+     * @param username
+     * @param password
+     * @param type
+     */
+    public void signUp(String username, String password, String type) {
+        if(type.equals("admin")) {
+            userPort.createUser(username, password, true);
+        }
+        else {
+            userPort.createUser(username, password, false);
+        }
+    }
+
+    /**
+     * Log a user with the given credentials
+     * @param username
+     * @param password
+     */
+    public void logIn(String username, String password) {
+        userPort.logIn(username, password);
+
+    }
+
+    public boolean isAuth() {
+        if(token == null) return false;
+        return true;
+    }
 }
--- UsersWS/src/main/java/fr/ensibs/users/UserServiceImpl.java
+++ UsersWS/src/main/java/fr/ensibs/users/UserServiceImpl.java
@@ -1,15 +1,25 @@
 package fr.ensibs.users;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 import javax.jws.WebService;
 import java.sql.*;
 
 @WebService(serviceName = "UserService", portName = "UserPort", endpointInterface = "fr.ensibs.users.UserService")
 public class UserServiceImpl implements UserService{
 
+    private static final String SECRET_KEY = "secret";
+    private static final String AUTHENTICATION_FAILED = "Authentication failed : Invalid credentials.";
+    private static final String AUTHENTICATION_SUCCESSFULL = "Authentication successful !";
     private static final String DATABASE_URL = "jdbc:sqlite:boulangerie.db";
-    private static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS users (\n"
+
+    private static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS users ( \n"
             + " id integer PRIMARY KEY, \n"
             + " username text UNIQUE, \n"
+            + " password text, \n"
             + " isAdmin integer NOT NULL \n"
             + ");";
 
@@ -19,21 +29,52 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void createUser(String username, String password, boolean isAdmin) {
-
         String sql = "INSERT INTO users (username, password, isAdmin) VALUES (?,?,?)";
         try (Connection conn = this.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setString(1, username);
-            pstmt.setString(2, password);
+            pstmt.setString(2, BCrypt.hashpw(password, BCrypt.gensalt()));
             if(isAdmin) pstmt.setInt(3, 1);
             else pstmt.setInt(3, 0);
             pstmt.executeUpdate();
+            System.out.println("User added successfully");
         } catch (SQLException e) { e.printStackTrace(); }
     }
 
     @Override
     public String logIn(String username, String password) {
+        String sql = "SELECT password, isAdmin FROM users WHERE username = ?";
+        try(Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, username);
+            ResultSet rs = pstmt.executeQuery();
+
+            if(rs.next() == false) {
+                System.out.println(AUTHENTICATION_FAILED);
+                return null;
+            }
+
+            String hashPassword = rs.getString("password");
+            int isAdmin = rs.getInt("isAdmin");
+
+            if(BCrypt.checkpw(password, hashPassword)) {
+                System.out.println(AUTHENTICATION_SUCCESSFULL);
+                try {
+                    Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+                    //String token = JWT.create()
+
+                } catch (JWTCreationException exception) {
+                    exception.printStackTrace();
+                }
+            } else {
+                System.out.println(AUTHENTICATION_FAILED);
+            }
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -42,6 +83,9 @@ public class UserServiceImpl implements UserService{
 
     }
 
+    /**
+     * Initialise the database
+     */
     private void initDB() {
         try (Connection conn = this.connect();
              Statement stmt = conn.createStatement()) {
@@ -50,6 +94,7 @@ public class UserServiceImpl implements UserService{
             System.out.println("USER TABLE created successfully.");
         } catch (SQLException e) { e.printStackTrace(); }
     }
+
 
     /**
      * Connect to the DATABASE_URL
